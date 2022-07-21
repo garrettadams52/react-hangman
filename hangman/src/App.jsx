ede689372523e469cdf148226476f8da30667c77
Garrett Adams <garrett.adams52@gmail.com>
2022-07-21 18:23:19 -0400

    update

--- hangman/src/App.jsx
+++ hangman/src/App.jsx
@@ -5,7 +5,7 @@ import data from './assets/data.js'
 import Display from "./components/display"
 import Button from './components/button'
 import Letters from './components/letters'
-import randomWord from './assets/random'
+import {randomWord} from './assets/random'
 import { useFetch } from "react-async"
 
 function App() {
@@ -13,31 +13,18 @@ function App() {
   let ind = Math.floor(Math.random() * (25-0+1))
   let blah = data[ind]
   let check = blah.split("")
-  
-  randomWord().then(rw=>{
-     check = rw.split("")
-   })
-  /////////////////////////////////
-  // const [users, setUsers] = useState([])
-  // const [user, setUser] = useState("")
-  // const [addNewUser, setAddNewUser] = useState("")
-  
-  // const getSetUsers = async () => {
-  //     const users = await getUsers();
-  //     console.log('users are', users);
-  //     // immutability
-  //     // users = users.user.list
-  //     setUsers(users.user.list)
-  // }
-  // useEffect(() => {
-  //   console.log('call endpoint')
-  //   getSetUsers();
-  // }, [addNewUser])
-  //////////////
   const [word, setWord] = useState(check)
   const [guesses, setGuesses] = useState([])
+  
+  const getRandomWord = async() => {
+    const word = await randomWord();
+    setWord(word.split(""))
+  }
 
-  console.log(word)
+  useEffect(
+    () => {getRandomWord();}, 
+    [],
+  )
   
   return (
     <div className="App">
--- hangman/src/assets/random.js
+++ hangman/src/assets/random.js
@@ -1,12 +1,15 @@
 import axios from 'axios'
 
-async function randomWord(){
-
+export const randomWord = async () =>{
+    try{
     let wordData = await axios.get("https://random-word-api.herokuapp.com/word")
     let word = wordData.data[0]
-
     return word
-    
-}
+    }catch(e){
+        throw e;
+    }
 
-export default randomWord
+    
+    
+};
+
